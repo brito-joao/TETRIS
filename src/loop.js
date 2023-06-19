cf284cb47024d6b20481415ec96f4b241b3ebb1f
João <joaobrit0000000000@gmail.com>
2023-06-19 21:56:33 +0100

    Add pixel style

--- src/loop.js
+++ src/loop.js
@@ -7,13 +7,13 @@ export function mainLoop(pixels){
     //display
     //red square block
     if(block_height-2>=0){
-        pixels[block_height][xposition].style.backgroundColor="red";
-        pixels[block_height][xposition+1].style.backgroundColor="red";
-        pixels[block_height-1][xposition].style.backgroundColor="red";
-        pixels[block_height-1][xposition+1].style.backgroundColor="red";
+        pixels[block_height][xposition].style.background="linear-gradient(45deg, rgba(0,0,0,1) 0%, rgba(255,3,0,1) 79%)";
+        pixels[block_height][xposition+1].style.background="linear-gradient(45deg, rgba(0,0,0,1) 0%, rgba(255,3,0,1) 69%)";
+        pixels[block_height-1][xposition].style.background="linear-gradient(45deg, rgba(0,0,0,1) 0%, rgba(255,3,0,1) 69%)";
+        pixels[block_height-1][xposition+1].style.background="linear-gradient(45deg, rgba(0,0,0,1) 0%, rgba(255,3,0,1) 69%)";
 
-        pixels[block_height-2][xposition].style.backgroundColor="white";
-        pixels[block_height-2][xposition+1].style.backgroundColor="white";
+        pixels[block_height-2][xposition].style.background="white";
+        pixels[block_height-2][xposition+1].style.background="white";
     }
     //movement
 
